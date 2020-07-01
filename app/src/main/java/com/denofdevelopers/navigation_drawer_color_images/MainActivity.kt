9a796478fb12772b5c12d4cd76acca4ec4d04e63
Aleksandar Gulevski <agulevski10@gmail.com>
2020-07-01 13:10:27 +0200

    Added navigation drawer and initialized it.

--- app/src/main/java/com/denofdevelopers/navigation_drawer_color_images/MainActivity.kt
+++ app/src/main/java/com/denofdevelopers/navigation_drawer_color_images/MainActivity.kt
@@ -2,11 +2,30 @@ package com.denofdevelopers.navigation_drawer_color_images
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initNavigationDrawer()
+    }
+
+    private fun initNavigationDrawer() {
+        val drawerToggle =
+            ActionBarDrawerToggle(this, navigationDrawer, toolbar, R.string.open, R.string.close)
+        navigationDrawer.addDrawerListener(drawerToggle)
+        navigationDrawer.isClickable = true
+        drawerToggle.syncState()
+        toolbarTitle.text = getString(R.string.den)
+        navigationView.itemIconTintList = null
+        drawerToggle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.colorWhite)
     }
 }
+
+
+

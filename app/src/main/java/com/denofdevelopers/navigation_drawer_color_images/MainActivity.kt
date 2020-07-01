976957b137fb41ae2758bf35a472fc3e0e574104
Aleksandar Gulevski <aleksandar.gulevski@haselt.com>
2020-07-01 11:12:49 +0200

    first commit

---
+++ app/src/main/java/com/denofdevelopers/navigation_drawer_color_images/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.denofdevelopers.navigation_drawer_color_images
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
